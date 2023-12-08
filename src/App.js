48056a6fa3108c4c496f4cefdf255aa05ac67620
Leo Wang <bluelionet@gmail.com>
2023-12-09 02:21:14 +0800

    feat: Implement YouTube shorts data fetching.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  /*
+   * Fetch YouTube shorts data from channel "THE FIRST TAKE".
+   */
+  useEffect(() => {
+    async function fetchYouTubeShortsData() {
+      try {
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_YOUTUBE_DATA_API_KEY}&part=snippet&fields=${encodeURIComponent('items/id/videoId')}&channelId=UC9zY_E8mcAo_Oq772LEZq8Q&type=video&videoDuration=short&order=date&maxResults=1`);
+        if (!response.ok) {
+          throw new Error(`YouTube Data API Error (${response.status}).`);
+        }
+        const json = await response.json();
+        console.log(json);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+
+    fetchYouTubeShortsData();
+  }, []);
+
+  return null;
 }
 
 export default App;
