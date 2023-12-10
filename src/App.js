fde9035fe786272fdf29f66b8f7426302ea8fa96
Leo Wang <bluelionet@gmail.com>
2023-12-10 22:33:31 +0800

    feat: Implement swipe and scroll functionality.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import { useEffect, useState } from 'react';
 import Short from './components/Short.js';
 import './App.css';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Mousewheel } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/mousewheel';
+
 function App() {
   const [videoIds, setVideoIds] = useState([]);
 
@@ -9,7 +14,7 @@ function App() {
     async function fetchShortsData() {
       try {
         // Fetch Shorts data from YouTube channel "THE FIRST TAKE".
-        const response = await fetch(`https://www.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_YOUTUBE_DATA_API_KEY}&part=snippet&fields=${encodeURIComponent('items/id/videoId')}&channelId=UC9zY_E8mcAo_Oq772LEZq8Q&type=video&videoDuration=short&order=date&maxResults=1`);
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_YOUTUBE_DATA_API_KEY}&part=snippet&fields=${encodeURIComponent('items/id/videoId')}&channelId=UC9zY_E8mcAo_Oq772LEZq8Q&type=video&videoDuration=short&order=date&maxResults=5`);
         if (!response.ok) {
           throw new Error(`YouTube Data API Error (${response.status}).`);
         }
@@ -27,9 +32,22 @@ function App() {
   }, []);
 
   // render Shorts from "videoIds".
-  return videoIds.map(videoId => (
-    <Short key={videoId} videoId={videoId} />
-  ));
+  return (
+    <Swiper
+      modules={[Mousewheel]}
+      direction="vertical"
+      slidesPerView="auto"
+      spaceBetween={24}
+      centeredSlides
+      mousewheel={{ forceToAxis: true }}
+    >
+      {videoIds.map(videoId => (
+        <SwiperSlide key={videoId}>
+          <Short videoId={videoId} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 }
 
 export default App;
